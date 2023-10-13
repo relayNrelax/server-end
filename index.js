9049db9a5f0c5a25c7f740df974625c5d66d643e
Bishal Deb <102280193+bishald0013@users.noreply.github.com>
2023-10-13 22:20:56 +0530

    url added

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ import alertRoute from './route/alertRoute.js';
 dotenv.config()
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: 'https://relynrelax.com/',
+}))
 
 connectDB()
 app.use(express.json({ limit: "30mb", extended: true }))
